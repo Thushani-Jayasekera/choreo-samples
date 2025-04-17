e8821b448b6d517a5d47fd7d3f9eac6017511ee4
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2025-04-17 22:11:35 +0530

    Add cert path

--- sample-service-with-tls/main.go
+++ sample-service-with-tls/main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,14 +15,22 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// Load server's certificate and private key
-	cert, err := tls.LoadX509KeyPair("server.crt", "server.key")
+	// Get cert path from ENV or default
+	certPath := "/home/ballerina"
+
+	// Construct full paths
+	serverCert := filepath.Join(certPath, "server.crt")
+	serverKey := filepath.Join(certPath, "server.key")
+	caCertPath := filepath.Join(certPath, "ca.pem")
+
+	// Load server certificate and key
+	cert, err := tls.LoadX509KeyPair(serverCert, serverKey)
 	if err != nil {
 		log.Fatalf("failed to load server cert/key: %v", err)
 	}
 
-	// Load CA cert (used to verify client certs)
-	caCert, err := ioutil.ReadFile("ca.pem")
+	// Load CA cert
+	caCert, err := ioutil.ReadFile(caCertPath)
 	if err != nil {
 		log.Fatalf("failed to read ca.pem: %v", err)
 	}
